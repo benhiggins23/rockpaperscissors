aa3785d4c1c2d028baff85cc32ddcbc9ed64e4d1
benhiggins23 <ben.higgins23@gmail.com>
2022-06-01 10:14:53 -0500

    Add 5 rounds and score counter

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ function computerPlay(){
 //prompt input from player
 let playerInput = prompt("Rock, Paper, or Scissors?");
 const playerSelection = playerInput.toLowerCase();
-
 //convert text from player to usable format 
-
 const computerSelection = computerPlay();
-
+let computerScore=0;
+let playerScore=0;
 
 function playRockPaperScissors(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
@@ -21,26 +20,44 @@ function playRockPaperScissors(playerSelection, computerSelection){
     }
     else if (playerSelection=="rock" && computerSelection=="paper"){
         console.log("You lose. Paper beats Rock");
+        computerScore++;
     }
     else if (playerSelection=="rock" && computerSelection=="scissors"){
         console.log("You win! Rock beats Scissors.");
+        playerScore++;
     }
     else if (playerSelection=="paper" && computerSelection=="rock"){
         console.log("You win! Paper beats Rock");
+        playerScore++;
     }
     else if (playerSelection=="paper" && computerSelection=="scissors"){
         console.log("You lost! Scissors beat Paper.");
+        computerScore++;
     }
     else if (playerSelection=="scissors" && computerSelection=="rock"){
         console.log("You lose. Rock beats Scissors.");
+        computerScore++;
     }
     else if (playerSelection=="scissors" && computerSelection=="paper"){
         console.log("You win! Scissors beats Paper.");
+        playerScore++;
     }
     else {
         alert("please play again and type in a correctly spelled choice");
     }
     
+}
+
+function game(){
+
+    for(let i=1; i<6; i++){
+        let playerInput = prompt("Rock, Paper, or Scissors?");
+        const playerSelection = playerInput.toLowerCase();
+        const computerSelection = computerPlay();
+        playRockPaperScissors(playerSelection,computerSelection);
+        console.log("You selected: "+playerSelection+"\nComputer Selected: "+computerSelection);
+        console.log("Your Score: "+playerScore+"\nComputer Score: "+computerScore);
+    }
 }
 
 //"You tied, please play again." playerSelection+"ties with"+computerSelection
@@ -59,6 +76,8 @@ function playRockPaperScissors(playerSelection, computerSelection){
     return "You," + playerResult +"!"+ winningSelection "beats" losingSelection
 
 */
-console.log(playerSelection)
-console.log(computerSelection)
-console.log(playRockPaperScissors(playerSelection,computerSelection))
+//console.log(playerSelection)
+//console.log(computerSelection)
+//console.log(playRockPaperScissors(playerSelection,computerSelection))
+//console.log(game())
+game();
