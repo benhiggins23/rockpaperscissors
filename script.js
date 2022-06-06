9f947f23255c241e9777a43d5c28b6426323d8b6
benhiggins23 <ben.higgins23@gmail.com>
2022-06-06 12:29:25 -0500

    add gui and divs to update score

--- script.js
+++ script.js
@@ -5,66 +5,95 @@ function computerPlay(){
     let randomChoice = computerChoice[Math.floor(Math.random()*computerChoice.length)]
     return randomChoice
 }
-const computerSelection = computerPlay();
 
 //prompt input from player
-const rockbtn = document.querySelectorAll('#rock');
-console.log(rockbtn);
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector("#paper");
+const scissorsbtn = document.querySelector("#scissors");
+//console.log(rockbtn);
 
-rockbtn.addEventListener("click", testFunction);
+rockbtn.addEventListener("click", () => game("rock"));
+paperbtn.addEventListener("click", () => game("paper"));
+scissorsbtn.addEventListener("click", () => game("scissors"));
 
-function testFunction(){
-  console.log("Hello World");
-
-}
-
-//let playerInput = prompt("Rock, Paper, or Scissors?");
-//const playerSelection = playerInput.toLowerCase();
-//convert text from player to usable format 
-
-
+function game(input){
+  let playerSelection = input;
+  const computerSelection = computerPlay();
+  playRockPaperScissors(playerSelection,computerSelection);
+  };
 
 let computerScore=0;
 let playerScore=0;
 
+function updateScore(whoWon){
+    const container = document.querySelector("#container");
+    
+        if (whoWon === "tie"){
+        container.innerHTML = "It's a tie";
+    } else if (whoWon ==="player"){
+        container.innerHTML = "You won";
+    }else{
+        container.innerHTML = "You lost";
+    }
+
+    const score = document.querySelector('#score');
+    score.innerHTML = "Your Score: "+playerScore+"\nComputer Score: "+computerScore;
+    
+
+
+}
+
 function playRockPaperScissors(playerSelection, computerSelection){
+    
+    
     if(playerSelection == computerSelection){
-        console.log("It's a tie")
+       // console.log("It's a tie")
+       updateScore("tie");
+      
     }
     else if (playerSelection=="rock" && computerSelection=="paper"){
-        console.log("You lose. Paper beats Rock");
-        computerScore++;
+        //console.log("You lose. Paper beats Rock");
+        ++computerScore;
+        updateScore("computer");
+        //console.log("Your Score: "+playerScore+"\nComputer Score: "+computerScore);
     }
     else if (playerSelection=="rock" && computerSelection=="scissors"){
-        console.log("You win! Rock beats Scissors.");
-        playerScore++;
+        //console.log("You win! Rock beats Scissors.");
+        ++playerScore;
+        updateScore("player");
+        
     }
     else if (playerSelection=="paper" && computerSelection=="rock"){
-        console.log("You win! Paper beats Rock");
+        //console.log("You win! Paper beats Rock");
         playerScore++;
+        updateScore("player");
     }
     else if (playerSelection=="paper" && computerSelection=="scissors"){
-        console.log("You lost! Scissors beat Paper.");
+        //console.log("You lost! Scissors beat Paper.");
         computerScore++;
+        updateScore("computer");       
     }
     else if (playerSelection=="scissors" && computerSelection=="rock"){
-        console.log("You lose. Rock beats Scissors.");
+        //console.log("You lose. Rock beats Scissors.");
         computerScore++;
+        updateScore("computer");
     }
     else if (playerSelection=="scissors" && computerSelection=="paper"){
-        console.log("You win! Scissors beats Paper.");
+        //console.log("You win! Scissors beats Paper.");
         playerScore++;
+        updateScore("player");
     }
     else {
         alert("please play again and type in a correctly spelled choice");
     }
     
 }
+
 /*
 function game(){
 
     for(let i=1; i<6; i++){
-        let playerInput = prompt("Rock, Paper, or Scissors?");
+        
         const playerSelection = playerInput.toLowerCase();
         const computerSelection = computerPlay();
         playRockPaperScissors(playerSelection,computerSelection);
